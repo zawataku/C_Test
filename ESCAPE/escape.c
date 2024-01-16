bc9b71e03580c18b8b869795669eac0baaf78389
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-01-16 14:34:16 +0900

    練習ファイル追加

---
+++ ESCAPE/escape.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("MS\t : Windows\n");
+    printf("Apple\t : Machintosh\n");
+    return 0;
+}
--- SELF_INTRODUCTION/S_I.c
+++ SELF_INTRODUCTION/S_I.c
@@ -3,4 +3,5 @@
 int main(void)
 {
     printf("こんにちは\n%sです\n誕生日は%d月%d日です", "ざわたく", 8, 19);
+    return 0;
 }
